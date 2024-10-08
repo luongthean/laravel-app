629902f185d261267d0b0f4b86571af8c3fa95ae
An Luong <an.luong@danaexperts.com>
2024-10-08 15:06:29 +0700

    [add] upload to s3

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        return view('welcome');
+    }
+
+    public function upload(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Store the image in the S3 bucket
+        $path = $request->file('image')->store('images', 's3');
+
+        // Get the full URL of the uploaded file
+        $url = Storage::disk('s3')->url($path);
+
+        // Return a response with the file URL or redirect to the form with the URL
+        return redirect()->route('upload.form')->with('url', $url);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [HomeController::class, 'home'])->name('upload.form');
+Route::post('/upload-image', [HomeController::class, 'upload']);
